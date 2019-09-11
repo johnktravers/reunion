988cb92a1dab7e341a3c41a0576117428b9b1948
John Travers <johnk.travers@gmail.com>
2019-09-10 22:44:22 -0600

    Add total_cost, breakout, and summary methods and tests

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -10,4 +10,26 @@ class Reunion
     @activities.push(activity)
   end
 
+  def total_cost
+    @activities.sum { |activity| activity.total_cost }
+  end
+
+  def breakout
+    owed = Hash.new(0)
+    @activities.each do |activity|
+      activity.participants.each do |name, paid|
+        owed[name] += activity.owed[name]
+      end
+    end
+    owed
+  end
+
+  def summary
+    string = ""
+    breakout.each do |name, owed|
+      string.concat("#{name}: #{owed}\n")
+    end
+    string.chomp
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -29,4 +29,47 @@ class ReunionTest < Minitest::Test
     assert_equal [@brunch, @drinks], @reunion.activities
   end
 
+  def test_total_cost
+    add_participants
+    @reunion.add_activity(@brunch)
+    assert_equal 60, @reunion.total_cost
+
+    @reunion.add_activity(@drinks)
+    assert_equal 180, @reunion.total_cost
+  end
+
+  def test_breakout
+    add_participants
+    add_activities
+
+    expected = {"Maria" => -10, "Luther" => -30, "Louis" => 40}
+    assert_equal expected, @reunion.breakout
+  end
+
+  def test_summary
+    add_participants
+    add_activities
+
+    expected = "Maria: -10\nLuther: -30\nLouis: 40"
+    assert_equal expected, @reunion.summary
+  end
+
+
+
+  #------------- Helper Methods -------------#
+
+  def add_participants
+    @brunch.add_participant("Maria", 20)
+    @brunch.add_participant("Luther", 40)
+
+    @drinks.add_participant("Maria", 60)
+    @drinks.add_participant("Luther", 60)
+    @drinks.add_participant("Louis", 0)
+  end
+
+  def add_activities
+    @reunion.add_activity(@brunch)
+    @reunion.add_activity(@drinks)
+  end
+
 end
